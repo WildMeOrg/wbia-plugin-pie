3f31f9fc34fd7c6c2c89f9b7daa8d99e5d09613a
karen chan <karen@karen-chan.com>
2020-09-25 15:57:12 +0100

    Remove all __future__ imports
    We added `__future__` imports for python 2.  Now that we are using
    python 3.6 or above, they are no longer necessary.
    
    The imports removed are:
    - absolute_import
    - division
    - print_function

--- wbia_pie/utils/tensorflow_losses.py
+++ wbia_pie/utils/tensorflow_losses.py
@@ -14,9 +14,6 @@
 # limitations under the License.
 # ==============================================================================
 """Implements various metric learning losses."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
