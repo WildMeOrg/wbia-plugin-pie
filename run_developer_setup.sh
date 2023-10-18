c1bebca399341b5c8cf97dc66646a0af8b0e22cf
tsubramanian <tamilselvan@gmail.com>
2023-10-18 21:42:56 +0530

    Update run_developer_setup.sh
    Fixing the "No space left error" on the GitHub runner workflow

--- run_developer_setup.sh
+++ run_developer_setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+export TMPDIR=/home/$USER/tmp
+mkdir $TMPDIR
+export PIP_CACHE_DIR=/home/$USER/tmp
+
 ./clean.sh
 python setup.py clean
 
-pip install -r requirements.txt
+
+pip install --cache-dir $TMPDIR -r requirements.txt
 
 python setup.py develop
 
